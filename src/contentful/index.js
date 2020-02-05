d053fc7cbe28b6efb28fab6f55fc934986cee83a
ure-asuslaptop <urosr95@gmail.com>
2020-02-05 16:29:59 +0100

    final

--- src/contentful/index.js
+++ src/contentful/index.js
@@ -2,5 +2,5 @@ import {createClient} from 'contentful'
 
 export const client = createClient({
     space: process.env.REACT_APP_SPACE,
-    accessToken: process.env.REACT_APP_ACCES_TOKEN
+    accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });

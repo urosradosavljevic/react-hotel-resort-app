9481abd96a3d2733cae08ff8b2ff57a7fc127930
Uros Radosavljevic <urosr95@gmail.com>
2021-02-08 09:29:32 +0100

    Refactor: Hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,24 +1,3 @@
-import { useState, useEffect } from "react";
-import { getRooms } from "../api";
+import { useRooms } from "./room";
 
-export const useRooms = () => {
-  const [rooms, setRooms] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getRooms();
-        const formated = formatRooms(response.items);
-        setRooms(formated);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    if (Array.isArray(rooms) && rooms.length === 0) {
-      fetchData();
-    }
-  }, [rooms]);
-
-  return { rooms, setRooms };
-};
+export { useRooms };
---
+++ src/hooks/room.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { getRooms } from "../api";
+
+export const useRooms = () => {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getRooms();
+        const formated = formatRooms(response.items);
+        setRooms(formated);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (Array.isArray(rooms) && rooms.length === 0) {
+      fetchData();
+    }
+  }, [rooms]);
+
+  return { rooms, setRooms };
+};
